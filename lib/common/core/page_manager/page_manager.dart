d2fe434a59241aef4aaf860603129668d7a12ce4
Mạnh Tài <manhtai831@gmail.com>
2022-01-09 21:23:16 +0700

    update core function

--- lib/common/core/page_manager/page_manager.dart
+++ lib/common/core/page_manager/page_manager.dart
@@ -1,9 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:shop_all_fe/common/core/page_manager/key_page.dart';
-import 'package:shop_all_fe/view/login/login_page.dart';
-import 'package:shop_all_fe/view/splash/splash_page.dart';
 
 List<GetPage> listPage = [
-  GetPage(name: KeyPage.initial_page, page: () => SplashPage()),
-  GetPage(name: KeyPage.login_page, page: () => LoginPage()),
+  GetPage(name: KeyPage.initial_page, page: () => Container()),
+  GetPage(name: KeyPage.login_page, page: () => Container()),
 ];
