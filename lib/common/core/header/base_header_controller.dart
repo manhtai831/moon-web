2385e64d867a12f7bc79b765ff53ab5fcba02e88
Mạnh Tài <manhtai831@gmail.com>
2022-01-17 17:34:11 +0700

    update core function

--- lib/common/core/header/base_header_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:shop_all_fe/common/core/base_controller.dart';
-
-class BaseHeaderController extends BaseController {
-  var visibleBanner0 = true.obs;
-  @override
-  Future initialData() async {}
-}
