2c56b484fadee2ee1286d223bf69f1b61cd58970
bxTruong <truongmagic9a@gmail.com>
2022-02-12 10:09:25 +0700

    truong tao padding

--- lib/common/core/widget/base_button.dart
+++ lib/common/core/widget/base_button.dart
@@ -10,28 +10,29 @@ class BaseButton extends StatelessWidget {
   final BorderRadiusGeometry? borderRadius;
   final TextStyle? style;
 
-  const BaseButton(
-      {Key? key,
-      this.title,
-      this.child,
-      this.onTab,
-      this.backgroundColor,
-      this.titleColor,
-      this.padding,
-      this.borderRadius,
-      this.style})
-      : super(key: key);
+  const BaseButton({
+    Key? key,
+    this.title,
+    this.child,
+    this.onTab,
+    this.backgroundColor,
+    this.titleColor,
+    this.padding,
+    this.borderRadius,
+    this.style,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: backgroundColor,
       borderRadius: borderRadius,
-      child: Padding(
-        padding: padding ?? const EdgeInsets.all(0),
-        child: InkWell(
-          onTap: () => onTab?.call(),
-          child: child ?? Text(title ?? '', style: style ?? TextStyle(color: titleColor)),
+      child: InkWell(
+        onTap: () => onTab?.call(),
+        child: Padding(
+          padding: padding ?? const EdgeInsets.all(0),
+          child: child ??
+              Text(title ?? '', style: style ?? TextStyle(color: titleColor)),
         ),
       ),
     );
---
+++ lib/common/core/widget/base_padding.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/cupertino.dart';
+//
+// class BasePadding extends StatefulElement{
+//   final double padding;
+//   //BasePadding(StatefulWidget widget) : super(widget);
+//
+//   @override
+//   Widget build() {
+//     return super.build();
+//   }
+// }
